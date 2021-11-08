677d5426d56344397e824c93e6ad97e0ef2ad212
hoangviet15ht <hoangviet15ht@gmail.com>
2021-11-08 20:50:42 +0700

    add fork

---
+++ kadai1.rb
@@ -0,0 +1,15 @@
+class Menu
+    attr_accessor :name
+    attr_accessor :price
+    def get_total_price(count)
+        total_price = count*@price
+        if count>2
+            total_price = total_price-10000
+        end
+        return "#{self.name} #{total_price}vnd"
+    end
+end
+menu1 = Menu.new
+menu1.name = "pho?"
+menu1.price = 30000
+puts menu1.get_total_price(3)
